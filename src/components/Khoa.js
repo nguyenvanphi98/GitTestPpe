69df81011a7d4e6c49bd4da06ef5907bd0fbf975
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-15 17:41:47 +0700

    cm

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -3,7 +3,7 @@ import React from 'react'
  const Khoa = () => {
     return (
         <div>
-            <p class="text-red-500 text-opacity-100 ...">Khoa</p>
+            <p class="text-red-500 text-opacity-100 ...">Khoa 123</p>
         </div>
     )
 }
