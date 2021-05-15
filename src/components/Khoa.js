84561cb5130068d2d2cbf79f8e2e7f7209918dc7
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-15 17:45:03 +0700

    cm

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -1,21 +1,11 @@
-import React from "react";
+import React from 'react'
 
-<<<<<<< HEAD
  const Khoa = () => {
     return (
         <div>
             <p class="text-red-500 text-opacity-100 ...">Khoa 123</p>
+            <button>Click </button>
         </div>
     )
 }
-=======
-const Khoa = () => {
-  return (
-    <div>
-      <div>Khoa master Git</div>
-      <div>Phi test git</div>
-    </div>
-  );
-};
->>>>>>> origin/master
 export default Khoa;
