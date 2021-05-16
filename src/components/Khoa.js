6114f1c81a1c27ffc03ae22fb412d231bdce92d3
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-16 21:13:13 +0700

    cm

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -1,23 +1,20 @@
 import React from "react";
 
 
-
-import React from 'react'
-
- const Khoa = () => {
-    return (
-        <div>
-            <p class="text-red-500 text-opacity-100 ...">Khoa 123</p>
-            <button>akjshd</button>
-            <textarea>pepe </textarea>
-            <p>Công ty PPE</p>
-            <div>
-      <div> Ok fine lên</div>
-      <div> Ok fine lên</div>
-      <div> Ok fine lên</div>
-      <div> Ok fine lên</div>
+const Khoa = () => {
+  return (
+    <div>
+      <p class="text-red-500 text-opacity-100 ...">Khoa 123</p>
+      <button>akjshd</button>
+      <textarea>pepe </textarea>
+      <p>Công ty PPE</p>
+      <div>
+        <div> Ok fine lên</div>
+        <div> Ok fine lên</div>
+        <div> Ok fine lên</div>
+        <div> Ok fine lên</div>
+      </div>
     </div>
-        </div>
-    )
-    }
+  );
+};
 export default Khoa;
