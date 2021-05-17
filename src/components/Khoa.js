b0df2556a30b6e06e1aa9d36dfaf302e766a1f9a
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-17 12:31:12 +0700

    cm

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -2,12 +2,20 @@ import "antd/dist/antd.css";
 import React from "react";
 
 const Khoa = () => {
+  const mystyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 50,
+    marginTop: 10,
+  };
   return (
-    <>
+    <div style={mystyle}>
       <p>PPE</p>
-      <p>PPE2</p>
-      <p>PPE1</p>
-    </>
+    </div>
   );
 };
 export default Khoa;
