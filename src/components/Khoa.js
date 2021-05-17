bb97fe6a1a01914c58546e38ba7a9bb64ba2bed9
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-17 09:44:53 +0700

    cm

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -1,36 +1,6 @@
-import {
-  HeartOutlined,
-  MinusCircleOutlined,
-  PlusOutlined,
-} from "@ant-design/icons";
-import { Button, Checkbox, Form, Input } from "antd";
 import "antd/dist/antd.css";
 import React from "react";
 
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
-};
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
-};
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 4 },
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 20 },
-  },
-};
-const formItemLayoutWithOutLabel = {
-  wrapperCol: {
-    xs: { span: 24, offset: 0 },
-    sm: { span: 20, offset: 4 },
-  },
-};
 const Khoa = () => {
   return <>
   <p>PPE</p>
