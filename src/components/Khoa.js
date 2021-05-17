2f851a963ecc350b35d147fb4ba48c6202de3f48
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-17 09:20:01 +0700

    cm

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -1,4 +1,4 @@
-import { HeartOutlined } from "@ant-design/icons";
+import { HeartOutlined, MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input } from "antd";
 import "antd/dist/antd.css";
 import React from "react";
@@ -11,6 +11,22 @@ const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 20 },
+  },
+};
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 20, offset: 4 },
+  },
+};
 const Khoa = () => {
   const onFinish = (values) => {
     console.log("Success:", values);
@@ -18,6 +34,9 @@ const Khoa = () => {
 
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
+  };
+  const onFinish1 = (values) => {
+    console.log("Received values of form:", values);
   };
   return (
     <>
@@ -67,6 +86,91 @@ const Khoa = () => {
         </Form.Item>
       </Form>
       <HeartOutlined />
+
+      <Form
+        name="dynamic_form_item"
+        {...formItemLayoutWithOutLabel}
+        onFinish={onFinish1}
+      >
+        <Form.List
+          name="names"
+          rules={[
+            {
+              validator: async (_, names) => {
+                if (!names || names.length < 2) {
+                  return Promise.reject(new Error("At least 2 passengers"));
+                }
+              },
+            },
+          ]}
+        >
+          {(fields, { add, remove }, { errors }) => (
+            <>
+              {fields.map((field, index) => (
+                <Form.Item
+                  {...(index === 0
+                    ? formItemLayout
+                    : formItemLayoutWithOutLabel)}
+                  label={index === 0 ? "Passengers" : ""}
+                  required={false}
+                  key={field.key}
+                >
+                  <Form.Item
+                    {...field}
+                    validateTrigger={["onChange", "onBlur"]}
+                    rules={[
+                      {
+                        required: true,
+                        whitespace: true,
+                        message:
+                          "Please input passenger's name or delete this field.",
+                      },
+                    ]}
+                    noStyle
+                  >
+                    <Input
+                      placeholder="passenger name"
+                      style={{ width: "60%" }}
+                    />
+                  </Form.Item>
+                  {fields.length > 1 ? (
+                    <MinusCircleOutlined
+                      className="dynamic-delete-button"
+                      onClick={() => remove(field.name)}
+                    />
+                  ) : null}
+                </Form.Item>
+              ))}
+              <Form.Item>
+                <Button
+                  type="dashed"
+                  onClick={() => add()}
+                  style={{ width: "60%" }}
+                  icon={<PlusOutlined />}
+                >
+                  Add field
+                </Button>
+                <Button
+                  type="dashed"
+                  onClick={() => {
+                    add("The head item", 0);
+                  }}
+                  style={{ width: "60%", marginTop: "20px" }}
+                  icon={<PlusOutlined />}
+                >
+                  Add field at head
+                </Button>
+                <Form.ErrorList errors={errors} />
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </>
   );
 };
