9c4d37280f89377483cc36e5a452c6226f8b7792
Bui-Van-Khoa <80700766+Bui-Van-Khoa@users.noreply.github.com>
2021-05-17 08:40:47 +0700

    cm

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { Timeline } from "antd";
-import { ClockCircleOutlined } from "@ant-design/icons";
+import { HeartOutlined } from "@ant-design/icons";
 import { Progress } from 'antd';
 import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
+import { Form, Input, Button, Checkbox } from "antd";
 
 const Khoa = () => {
   return (
     <>
-      <div style={{ width: 170 }}>
-        <Progress percent={30} size="large" />
-        <Progress percent={50} size="large" status="active" />
-        <Progress percent={70} size="large" status="exception" />
-        <Progress percent={100} size="large" />
-      </div>
+      <HeartOutlined />
     </>
   );
 };
