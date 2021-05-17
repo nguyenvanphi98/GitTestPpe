94b43790fbbe5067a82905696a12a366cf64ae67
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-17 09:28:52 +0700

    commit

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -165,11 +165,6 @@ const Khoa = () => {
             </>
           )}
         </Form.List>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
       </Form>
     </>
   );
