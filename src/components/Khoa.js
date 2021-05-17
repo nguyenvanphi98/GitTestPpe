fd9b32faf542c9a96622222f3ef352ad12b6d6fb
BuiVanKhoa <buivankhoa212@gmail.com>
2021-05-17 08:14:33 +0700

    3rd

--- src/components/Khoa.js
+++ src/components/Khoa.js
@@ -1,19 +1,15 @@
 import React from "react";
 
-
 const Khoa = () => {
   return (
     <div>
       <p class="text-red-500 text-opacity-100 ...">Khoa 123</p>
-      <button>akjshd</button>
-      <textarea>pepe </textarea>
       <p>Công ty PPE</p>
       <div>
-
         <p>ahihi</p>
-       <p> Khoa Van Bui </p>
+        <p> Khoa Van Bui </p>
       </div>
-       <p>Bui Van Khoa </p>
+      <p>Công ty PPE </p>
     </div>
   );
 };
